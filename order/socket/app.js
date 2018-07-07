fc905892481d4b4c2c370b8633f1dbf481689291
Adelchino Simone <seconddesire@gmail.com>
2018-07-07 08:49:51 +0200

    Added amqplib to the OrderSocket listening order > on.email.sent

--- order/socket/app.js
+++ order/socket/app.js
@@ -1,6 +1,26 @@
-const amqp = require("amqp");
+//const amqp = require("amqp");
 const server = require('http').createServer();
 
+var amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://rabbitmq', function(err, conn) {
+  conn.createChannel(function(err, ch) {
+    var ex = 'order';
+
+    ch.assertExchange(ex, 'direct', {durable: false});
+
+    ch.assertQueue('on.email.sent', {exclusive: true}, function(err, q) {
+      console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue);
+      ch.bindQueue(q.queue, ex, 'on.email.sent');
+
+      ch.consume(q.queue, function(msg) {
+        console.log(" [x] %s", msg.content.toString());
+      }, {noAck: true});
+    });
+  });
+});
+
+/*
 // Start RabbitMQ connection
 
 var amqpClient = amqp.createConnection({ host: 'rabbitmq' });
@@ -12,6 +32,7 @@ amqpClient.on('error', function (e) {
 amqpClient.on('ready', function () {
   console.log("RabbitMQ connection ready");
 });
+*/
 
 // Start Socket.io server
 
