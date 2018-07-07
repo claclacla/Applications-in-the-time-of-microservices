faf7704114f8534692db421bc410eba975ad4206
Adelchino Simone <seconddesire@gmail.com>
2018-07-07 23:47:29 +0200

    Added Routing.js

---
+++ js/lib/MessageBroker/Routing.js
@@ -0,0 +1,15 @@
+class Routing {
+  static get Wide() {
+    return "Wide";
+  }
+
+  static get Explicit() {
+    return "Explicit";
+  }
+
+  static get PatternMatching() {
+    return "PatternMatching";
+  }
+}
+
+module.exports = Routing
